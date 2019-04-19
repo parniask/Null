e974b451150428af20b2d5751de8b9b3df9fab8e
parniask <Psk0021531900>
2019-04-19 14:18:02 +0330

    List changed

--- 1st_pythoncode.py
+++ 1st_pythoncode.py
@@ -1,4 +1,7 @@
-list01 = [2, 4, 6, 8]
-Avg = sum(list01)/len(list01)
-print(Avg)
+list01 = [3, 7, 5.2, 12.8]
+avg = sum(list01)/len(list01)
+print(avg)
+
+print("max value of list01 is: ",max(list01))
+print("min value of list01 is: ",min(list01))
 
