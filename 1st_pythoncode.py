3118d6fdc3383d410ce39f533b0e9ac57bbc8122
parniask <Psk0021531900>
2019-04-19 16:05:52 +0330

    2list

--- 1st_pythoncode.py
+++ 1st_pythoncode.py
@@ -1,5 +1,7 @@
 list01 = [3, 7, 5.2, 12.99, 12, 13, 43, 8]
 print(len(list01))
-listleft=(list01[:4])
-listright=(list01[4:])
-print(max(listleft)+max(listright))
+mid = len(list01) // 2
+listleft = (list01[:mid])
+listright = (list01[mid:])
+answer = max(listleft) + max(listright)
+print(answer)
